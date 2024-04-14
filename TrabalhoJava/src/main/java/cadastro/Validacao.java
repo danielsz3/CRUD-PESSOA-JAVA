a9040c52404980f9f53278238ddac7d6849342a7
Daniel Oliveira <danielmo352@gmail.com>
2024-04-14 11:54:35 -0300

    zap

--- TrabalhoJava/src/main/java/cadastro/Validacao.java
+++ TrabalhoJava/src/main/java/cadastro/Validacao.java
@@ -12,15 +12,20 @@ public class Validacao {
                 throw new RuntimeException("Nome inválido: deve ter pelo menos 6 caracteres e conter apenas letras");
             }
 
+
             if (!email.contains("@") || !email.endsWith(".com")) {
                 throw new RuntimeException("E-mail inválido");
             }
+
 
             if (!(telefone.length() == 11)) {
                 throw new RuntimeException("Digite um número de telefone válido");
             }
 
         } catch (RuntimeException e) {
+            throw e;
+
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
