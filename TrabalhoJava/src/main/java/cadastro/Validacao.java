4dc496753f5e9b93587701545a0f0afff90478b5
Daniel Oliveira <danielmo352@gmail.com>
2024-04-14 10:57:47 -0300

    TryCatch

--- TrabalhoJava/src/main/java/cadastro/Validacao.java
+++ TrabalhoJava/src/main/java/cadastro/Validacao.java
@@ -12,17 +12,24 @@ public class Validacao {
 
         // Validação dos campos
 
-        if (!(nome.length() >= 6) || !nome.matches("^[a-zA-Zà-ü\\s]+$")) {
-            throw new Exception("Nome inválido: deve ter pelo menos 6 caracteres e conter apenas letras");
-        }
+        try {
 
-        if (!email.contains("@") || !email.endsWith(".com")) {
-            throw new Exception("E-mail inválido");
-        }
 
-        if (!(telefone.length() == 11)) {
-            throw new Exception("Digite um número de telefone válido");
-        }
+            if (!(nome.length() >= 6) || !nome.matches("^[a-zA-Zà-ü\\s]+$")) {
+                throw new Exception("Nome inválido: deve ter pelo menos 6 caracteres e conter apenas letras");
+            }
 
+            if (!email.contains("@") || !email.endsWith(".com")) {
+                throw new Exception("E-mail inválido");
+            }
+
+            if (!(telefone.length() == 11)) {
+                throw new Exception("Digite um número de telefone válido");
+            }
+
+        } catch (Exception e) {
+
+            throw new RuntimeException(e);
+        }
     }
 }
